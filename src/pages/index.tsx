a24326306acef9aa3f6d5df39913123806cb9935
Tabu Hana <taboodog@hotmail.com>
2022-07-05 16:41:27 -0700

    random number gen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,21 @@
+import { getOptionsForVote } from "../utils/getRandomPokemon";
 import type { NextPage } from "next";
-import Head from "next/head";
+import { useEffect, useState } from "react";
 import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
   const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
+
+  const [first, second] = getOptionsForVote();
 
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Amiibo would win in a fight?</div>
       <div className="p-4"/>
       <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-        <div>test</div>
+        <div>{first}</div>
         <div className="p-8">vs</div>
-        <div>test</div>
+        <div>{second}</div>
       </div>
     </div>
   );
