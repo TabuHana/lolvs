c3fd2f1fcb0c0cdefcc949b5b70cfd2003ef741e
Tabu Hana <taboodog@hotmail.com>
2022-07-05 13:09:59 -0700

    basic UI for fighting section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,15 @@ const Home: NextPage = () => {
   const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
 
   return (
-    <div className="">Hello World</div>
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-2xl text-center">Which Amiibo would win in a fight?</div>
+      <div className="p-4"/>
+      <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
+        <div>test</div>
+        <div className="p-8">vs</div>
+        <div>test</div>
+      </div>
+    </div>
   );
 };
 
